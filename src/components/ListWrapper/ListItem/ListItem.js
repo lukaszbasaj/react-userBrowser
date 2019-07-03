a053e895d73070188ca5af89aab2518505d6d4e6
Łukasz Basaj <lbasaj@gmail.com>
2019-07-03 15:40:43 +0200

    minor fixes

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -15,7 +15,7 @@ const ListItem = ({
             <div>
                 <h2 className="listItem__name">{name}</h2>
                 <p className="listItem__description">{description}</p>
-                <a href={twitterLink} className="listItem__button">visit profile</a>
+                <a href={twitterLink} target="_blank" rel="noopener noreferrer" className="listItem__button">visit profile</a>
             </div>
         </li>
     );
