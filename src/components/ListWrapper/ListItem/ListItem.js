08da8ab7a068dfe365428ac5de3b1987c449d7c6
Łukasz Basaj <lbasaj@gmail.com>
2019-07-03 12:04:30 +0200

    add styles to listItem

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './ListItem.css';
+import personOneImage from '../../../assets/images/person_one.jpeg'
+
 
 
 const ListItem = () => (
-    <li className="listItemWrapper">
-        item 1
+    <li className="listItem__wrapper">
+        <img src={personOneImage} class="listItem__image" alt="First Person" />
+        <div>
+            <h2 className="listItem__name">video</h2>
+            <p className="listItem__description">qwertyui</p>
+            <button className="listItem__button">visit twitter page</button>
+        </div>
     </li>
 );
 
